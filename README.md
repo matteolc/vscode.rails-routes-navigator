16dad99d53500dd340b5a3bb01d57e198a2c46cd
Matteo La Cognata <matteo.lacognata@capdesk.com>
2023-05-24 17:57:49 +0200

    Refac and name change

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Rails Routes Navigator
+# Rails Routes Search
 
 This is a fork of [this](https://github.com/makicamel/rails-routes-navigator)
 extension.
@@ -6,7 +6,7 @@ extension.
 ## Features
 
 Display and search your Rails project routes. Hit command
-`Rails Routes Navigator: Show Routes`
+`Rails Routes Search: Open`.
 
 ## Requirements
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,11 @@
-// // The module 'vscode' contains the VS Code extensibility API
-// // Import the module and reference it with the alias vscode in your code below
-// import * as vscode from 'vscode';
-
-// // This method is called when your extension is activated
-// // Your extension is activated the very first time the command is executed
-// export function activate(context: vscode.ExtensionContext) {
-
-// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-// 	// This line of code will only be executed once when your extension is activated
-// 	console.log('Congratulations, your extension "rails-routes" is now active!');
-
-// 	// The command has been defined in the package.json file
-// 	// Now provide the implementation of the command with registerCommand
-// 	// The commandId parameter must match the command field in package.json
-// 	let disposable = vscode.commands.registerCommand('rails-routes.helloWorld', () => {
-// 		// The code you place here will be executed every time your command is executed
-// 		// Display a message box to the user
-// 		vscode.window.showInformationMessage('Hello World from rails-routes!');
-// 	});
-
-// 	context.subscriptions.push(disposable);
-// }
-
-// // This method is called when your extension is deactivated
-// export function deactivate() {}
-
 import * as vscode from 'vscode'
 import * as path from 'path'
 import {Routes} from './routes'
-import {Contents} from './contents'
 
 export async function activate(context: vscode.ExtensionContext) {
   let currentPanel: vscode.WebviewPanel | undefined = undefined
   let disposable = vscode.commands.registerCommand(
-    'rails-routes-navigator.search',
+    'rails-routes-search.open',
     () => {
       if (currentPanel) {
         currentPanel.reveal(vscode.ViewColumn.Two)
@@ -51,7 +23,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
       currentPanel = vscode.window.createWebviewPanel(
         'railsRoutesNavigator',
-        'Rails Routes Navigator',
+        'Rails Routes Search',
         vscode.ViewColumn.Two,
         {
           enableScripts: true,
@@ -178,7 +150,7 @@ function getWebviewContent(
   <title>Rails Routes Navigator</title>
 </head>
 <body>
-  <h1>Rails Routes Navigator</h1>
+  <h1>Rails Routes Search</h1>
   <div id="inputPanel">
     <button id="refreshButton" type="button">
       Refresh
